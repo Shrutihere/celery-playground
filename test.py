36b51f3f1df3d73ca63e370f318a884d1e8270b2
Shrutihere <jainshruti2605@gmail.com>
2022-09-29 12:11:11 +0530

    integrating fastapi

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-from tasks import scheduler
+from worker.celery_worker import test_celery
 from datetime import datetime, timedelta
 
 
 
 eta = datetime.utcnow() + timedelta(seconds=10)
-res = scheduler.apply_async((10,1),eta=eta)
+res = test_celery.apply_async([10],eta=eta)
 print(res.get(timeout=10))
